3f339982d77d5858e41f9f05092d8294c868e7f3
Ahristi <thomasvmholland@outlook.com>
2026-05-31 15:53:54 +1000

    begin i2c implementation on bms v2

---
+++ Software/BMS/Core/Inc/telemetry.h
@@ -0,0 +1,17 @@
+#ifndef __TELEMETRY_H__
+#define __TELEMETRY_H__
+#include <stdint.h>
+
+
+
+typedef struct{
+    uint16_t battery_voltage;
+    uint16_t battery_temp;
+}TELEMETRY_Handler_t;
+
+
+
+
+
+
+#endif
